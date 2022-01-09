dfa84d342588054a767e21120de3a7e4e381f53b
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-01-09 18:48:56 +0100

    single post design

--- src/components/templates/Dashboard/Dashboard.js
+++ src/components/templates/Dashboard/Dashboard.js
@@ -3,22 +3,86 @@ import img from 'assets/images/1.jpg';
 import heart from 'assets/images/heart-icon.svg';
 import styled from 'styled-components';
 
+const PostsContainer = styled.section`
+    margin-top: 3rem;
+`;
+
+const SinglePost = styled.article`
+    display: flex;
+    flex-direction: column;
+    width: 90vw;
+    margin: 0 auto;
+    margin-top: ${({primary}) => primary ? '0' : '4rem'};
+`;
+
+const UserInfo = styled.div`
+    display: flex;
+    align-items: center;
+    margin-top: 1rem;
+
+    span {
+        margin-left: 2.5rem;
+        font-weight: 300;
+        font-size: .8rem;
+    }
+`;
+
+const Text = styled.p`
+    margin-top: 1rem;
+    font-size: 1rem;
+    font-weight: 300;
+    line-height: 1.3rem;
+`;
+
+const Picture = styled.img`
+    width: 100%;
+    height: auto;
+`;
+
+const Likes = styled.div`
+    display: flex;
+    align-items: center;
+    margin-top: 1rem;
+
+    img {
+        cursor: pointer;
+    }
+
+    span {
+        margin-left: .8rem;
+    }
+`;
+
 const Dashboard = () => {
     return ( 
         <>
             <Navigation />
-            <div>
-                <img src={img} alt="img" />
-                <div>
-                    <h2>Dawson</h2>
-                    <span>In Las Vegas</span>
-                </div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero eligendi error perferendis ad corrupti facilis nulla laboriosam iste culpa repellat!</p>
-                <div>
-                    <img src={heart} alt="heart" />
-                    <span>17 People like it</span>
-                </div>
-            </div>
+            <PostsContainer>
+                <SinglePost primary>
+                    <Picture src={img} alt="img" />
+                    <UserInfo>
+                        <h2>Dawson</h2>
+                        <span>In Las Vegas</span>
+                    </UserInfo>
+                    <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero eligendi error perferendis ad corrupti facilis nulla laboriosam iste culpa repellat!</Text>
+                    <Likes>
+                        <img src={heart} alt="heart" />
+                        <span>17 People like it</span>
+                    </Likes>
+                </SinglePost>
+                <SinglePost>
+                    <Picture src={img} alt="img" />
+                    <UserInfo>
+                        <h2>Dawson</h2>
+                        <span>In Las Vegas</span>
+                    </UserInfo>
+                    <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero eligendi error perferendis ad corrupti facilis nulla laboriosam iste culpa repellat!</Text>
+                    <Likes>
+                        <img src={heart} alt="heart" />
+                        <span>17 People like it</span>
+                    </Likes>
+                </SinglePost>
+            </PostsContainer>
         </>
      );
 }
