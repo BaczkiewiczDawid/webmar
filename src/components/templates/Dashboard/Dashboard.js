b0b298fc450d8e96b5014d3dad50d8117c3eb6e0
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-01-09 18:29:08 +0100

    posts

--- src/components/templates/Dashboard/Dashboard.js
+++ src/components/templates/Dashboard/Dashboard.js
@@ -1,9 +1,24 @@
 import Navigation from 'components/templates/Navigation/Navigation';
+import img from 'assets/images/1.jpg';
+import heart from 'assets/images/heart-icon.svg';
+import styled from 'styled-components';
 
 const Dashboard = () => {
     return ( 
         <>
             <Navigation />
+            <div>
+                <img src={img} alt="img" />
+                <div>
+                    <h2>Dawson</h2>
+                    <span>In Las Vegas</span>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero eligendi error perferendis ad corrupti facilis nulla laboriosam iste culpa repellat!</p>
+                <div>
+                    <img src={heart} alt="heart" />
+                    <span>17 People like it</span>
+                </div>
+            </div>
         </>
      );
 }
